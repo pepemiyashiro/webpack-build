c1817733c9a8d496d8540ffda57cd0b1bf266287
Pepe Miyashiro <jmiyashiro@authxconsulting.com>
2017-02-23 09:30:41 -0500

    [upgrade] Sass integration to the build

--- webpack/webpack.plugins.js
+++ webpack/webpack.plugins.js
@@ -1,13 +1,14 @@
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
-// const extractSass = new ExtractTextPlugin({
-//     filename: "[name].scss"
-// });
-
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 let pluginsConfig = [
   // Add Plugins needed here
   // extractSass
+  new ExtractTextPlugin(
+    {
+      filename: "../css/[name].css",
+      allChunks: true
+    }
+  )
 ];
 
 
